6435a133066c2121566368cbe9b1c72406cb4a21
Rahul <rahul222366@gmail.com>
2020-02-20 12:20:45 +0530

    App Upload

---
+++ app/src/main/java/com/rahul/darktheme/OtherActivity.kt
@@ -0,0 +1,20 @@
+package com.rahul.darktheme
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
+import kotlinx.android.synthetic.main.activity_main.*
+
+class OtherActivity : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_other)
+
+
+    }
+
+}
